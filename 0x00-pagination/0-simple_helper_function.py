64b2437d8603a24f2c0114c778c6f6a8df159d84
Franklin Obasi <franklinobasy@gmail.com>
2023-02-20 12:52:42 +0100

    Task 0: Simple helper function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -7,5 +7,5 @@ from typing import Tuple
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Retrieves the index range from a given page and page size.
     """
-    
+
     return ((page - 1) * page_size, ((page - 1) * page_size) + page_size)
