0754815c932a86d64b283a79dfa56c583f17cf6c
Franklin Obasi <franklinobasy@gmail.com>
2023-02-20 12:53:06 +0100

    Task 1: Simple pagination.

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -10,7 +10,7 @@ from typing import List, Tuple
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Retrieves the index range from a given page and page size.
     """
-    
+
     return ((page - 1) * page_size, ((page - 1) * page_size) + page_size)
 
 
