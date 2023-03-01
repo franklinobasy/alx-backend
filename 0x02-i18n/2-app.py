fb8f67ae0edc6e3d9136f76d141c0ba071fcc886
Franklin Obasi <franklinobasy@gmail.com>
2023-03-01 18:26:30 +0100

    Task 2: Get locale from request

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -31,7 +31,7 @@ def get_locale():
 @app.route('/')
 def index():
     '''default route'''
-    return render_template("1-index.html",)
+    return render_template("2-index.html",)
 
 
 if __name__ == "__main__":
