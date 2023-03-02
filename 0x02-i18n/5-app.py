75cff0414ac1a7ea12795e8820520d1dacd85d44
Franklin Obasi <franklinobasy@gmail.com>
2023-03-02 02:03:58 +0100

    Task 6: Use user locale

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -56,6 +56,11 @@ def get_locale() -> str:
     locale = request.args.get('locale')
     if locale in app.config['LANGUAGES']:
         return locale
+    if g.user and g.user['locale'] in app.config["LANGUAGES"]:
+        return g.user['locale']
+    header_locale = request.headers.get('locale', '')
+    if header_locale in app.config["LANGUAGES"]:
+        return header_locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
